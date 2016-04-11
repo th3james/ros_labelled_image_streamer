1dbde649acdb2a4d9033f7bb94f86688d4b9e8ef
Dali Turtlebot <robot@dali.local>
2016-04-11 10:05:14 +0000

    Write files based on user inputted current command

--- image_writer.py
+++ image_writer.py
@@ -6,6 +6,7 @@ __version__=  '0.1'
 import sys, time
 
 import numpy as np
+import curses
 
 import cv2
 from cv_bridge import CvBridge
@@ -13,15 +14,29 @@ from cv_bridge import CvBridge
 import roslib
 import rospy
 
+from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Image
 
 VERBOSE=True
 INPUT_TOPIC="/camera/rgb/image_raw/"
 OUTPUT_DIR="/media/sdmount/sdcard/images/"
+SECONDS_BETWEEN_IMAGES=1.0
+
+
+def get_current_command(stdscr):
+  mappings = {
+    119: "Forward",
+    97: "Left",
+    100: "Right",
+  }
+  curses.flushinp()
+  ch = stdscr.getch()
+  return mappings.get(ch, None)
 
 class image_feature:
 
-  def __init__(self):
+  def __init__(self, stdscr):
+    self.stdscr = stdscr
     rospy.init_node('image_feature')
       
     self.subscriber = rospy.Subscriber(INPUT_TOPIC,
@@ -32,23 +47,32 @@ class image_feature:
   def callback(self, ros_data):
     '''Callback function of subscribed topic.
     Streams images to disk when button is depressed'''
-    if rospy.Time.now() > self.last + rospy.Duration(1.0):
+    if rospy.Time.now() > self.last + rospy.Duration(SECONDS_BETWEEN_IMAGES):
       self.last = rospy.Time.now()
+      
+      command = get_current_command(self.stdscr)
 
-      if VERBOSE :
-        print 'received image of size: {0}*{1}'.format(ros_data.height, ros_data.width)
+      if command is not None:
+        if VERBOSE :
+          print 'received image of size: {0}*{1}'.format(ros_data.height, ros_data.width)
 
-      cv_image = CvBridge().imgmsg_to_cv2(ros_data, desired_encoding="passthrough")
-      with open(OUTPUT_DIR + str(self.last.secs) + '.jpg', 'w+') as file:
-          cv2.imwrite(file.name, cv_image)
+        cv_image = CvBridge().imgmsg_to_cv2(ros_data, desired_encoding="passthrough")
+        filename = OUTPUT_DIR + command + str(self.last.secs) + '.jpg'
+        self.stdscr.addstr(filename)
+        with open(filename, 'w+') as file:
+            cv2.imwrite(file.name, cv_image)
 
-def main(args):
-  '''Initializes and cleanup ros node'''
-  ic = image_feature()
+
+def curses_main(stdscr):
+  ic = image_feature(stdscr)
   try:
     rospy.spin()
   except KeyboardInterrupt:
     print "Shutting down ROS Image feature detector module"
+
+def main(args):
+  '''Initializes and cleanup ros node'''
+  curses.wrapper(curses_main)
 
 if __name__ == '__main__':
     main(sys.argv)
