819f72e7fba1a72303d21f6654df272494a2f4a1
James Cox-Morton <th3james@fastmail.fm>
2016-04-12 13:37:52 +0100

    Correctly load training data into matrix

--- training/train.py
+++ training/train.py
@@ -17,13 +17,11 @@ train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
 init = tf.initialize_all_variables()
 data = training_data.load_data()
-import pdb; pdb.Pdb().set_trace()
 
 with tf.Session() as sess:
   sess.run(init)
-  for i in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+  batch_xs, batch_ys = data['train']
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
   correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
--- training/training_data.py
+++ training/training_data.py
@@ -1,29 +1,38 @@
-import cv2
 import glob
 import random
+import cv2
+import numpy as np
 
 TRAINING_DATA_PATH = "/Users/th3james/src/curricular/ros_labelled_image_streamer/training/training-data/"
-example_name = "Forward-1460378468.jpg"
+IMAGE_POINTS = 921600
+OUTPUT_CLASSES = 3
+RECORDS_TO_LOAD = 10
 
 def load_data():
   print(TRAINING_DATA_PATH + '*jpg')
   image_names = glob.glob(TRAINING_DATA_PATH + '*jpg')
   random.shuffle(image_names)
   
+  images_data = np.zeros(
+    (RECORDS_TO_LOAD, IMAGE_POINTS), dtype=np.uint8
+  )
+  labels = np.zeros(
+    (RECORDS_TO_LOAD, OUTPUT_CLASSES), dtype=np.uint8
+  )
+
+  for i in range(0, 10):
+    images_data[i] = load_image(image_names[i])
+    labels[i] = extract_label(image_names[i])
+
   return {
-    'train': map(load_image_and_label, image_names[0:2]),
-    'cross': map(load_image_and_label, image_names[2:2]),
-    'test': map(load_image_and_label, image_names[3:4]),
+    'train': [images_data[0:6], labels[0:6]],
+    'cross': [images_data[6:8], labels[6:8]],
+    'test': [images_data[8:10], labels[8:10]],
   }
-  
-def load_image_and_label(path):
-  return [
-    load_image(path), extract_label(path)
-  ]
 
 def load_image(path):
   img = cv2.imread(path)
-  return img.flatten()
+  return img.flatten().transpose()
 
 ACTION_TO_LABEL = {
   'Left': [1, 0, 0],
