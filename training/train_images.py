0e4f8dbf74998dd07f840a385d1a21e06bf4aa41
James Cox-Morton <th3james@fastmail.fm>
2016-04-25 21:01:35 +0100

    Correct dimensions for convolutional neural network

--- training/train_images.py
+++ training/train_images.py
@@ -43,35 +43,35 @@ def bias_variable(shape):
   return tf.Variable(initial)
 
 def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+  return tf.nn.conv2d(x, W, strides=[1, 2, 2, 1], padding='SAME')
 
 def max_pool_2x2(x):
   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
                         strides=[1, 2, 2, 1], padding='SAME')
 
 
-W_conv1 = weight_variable([5, 5, 3, 32])
-b_conv1 = bias_variable([32])
+W_conv1 = weight_variable([5, 5, 3, 10])
+b_conv1 = bias_variable([10])
 
 x_image = tf.reshape(x, [-1]+training_data.IMAGE_DIMENSIONS)
 
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
-W_conv2 = weight_variable([5, 5, 32, 64])
-b_conv2 = bias_variable([64])
+W_conv2 = weight_variable([5, 5, 10, 20])
+b_conv2 = bias_variable([20])
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
-W_fc1 = weight_variable([7 * 7 * 64, 1024])
-b_fc1 = bias_variable([1024])
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+W_fc1 = weight_variable([30 * 40 * 20, 360])
+b_fc1 = bias_variable([360])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 30*40*20])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder(tf.float32)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-W_fc2 = weight_variable([1024, training_data.OUTPUT_CLASSES])
+W_fc2 = weight_variable([360, training_data.OUTPUT_CLASSES])
 b_fc2 = bias_variable([training_data.OUTPUT_CLASSES])
 
 y_conv=tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
@@ -84,8 +84,16 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 with tf.Session() as sess:
   sess.run(tf.initialize_all_variables())
   
-  batch_xs, batch_ys = data['train']
-  train_step.run(feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
+  batchSize = 5
+  xs, ys = data['train']
+  for i in xrange(0, len(xs), batchSize):
+    batch_xs = xs[i:i+batchSize]
+    batch_ys = ys[i:i+batchSize]
+    train_step.run(feed_dict={
+      x: batch_xs, y_: batch_ys, keep_prob: 0.5})
+    print('.')
   
   print("test accuracy %g"%accuracy.eval(feed_dict={
-    x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+    x: data['test'][0], y_: data['test'][1], keep_prob: 1.0}))
+
+
